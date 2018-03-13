3606a46304798f3ebbf16b8bbf1b3eeb0f3a3fa5
Wes Mason <wes@1stvamp.org>
2018-03-13 12:40:09 +0000

    Fix path to copy config file from

--- .profile.d/00-set-datadog-logging-config.sh
+++ .profile.d/00-set-datadog-logging-config.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
 
-APP_DIR=`cd $(dirname $0); cd ..; pwd`
-
 mkdir -p /app/.apt/etc/datadog-agent/conf.d/logging
-cp $APP_DIR/logging-config.yml /app/.apt/etc/datadog-agent/conf.d/logging/config.yml
+cp logging-config.yml /app/.apt/etc/datadog-agent/conf.d/logging/config.yml
